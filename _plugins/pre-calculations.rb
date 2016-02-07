8bb6128a884f0366acdbc49813a1edb271bacede
Martin Nordholts <enselic@gmail.com>
2016-02-07 23:45:32 +0100

    wip

--- _plugins/pre-calculations.rb
+++ _plugins/pre-calculations.rb
@@ -1,7 +1,12 @@
 module SetOfSkills
   class PreCalculations < Jekyll::Generator
     def generate(site)
-    	site.data['nordholts'] = "det funkar"
+    	tag_counts = []
+    	site.tags.each_key do |tagName|
+    		tag_counts << { tag: tagName, count: site.tags[tagName].size, posts: site.tags[tagName]}
+    	end
+    	tag_counts.sort_by! { |entry| entry[:count] }
+    	site.data['tag_counts'] = tag_counts.reverse
     end
   end
 end
