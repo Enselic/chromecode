dc9c1a7ddd1d19128f69d7b59812cc5db94ee9f5
Martin Nordholts <enselic@gmail.com>
2016-02-08 00:02:27 +0100

    Sort tags by tag count

--- _plugins/pre-calculations.rb
+++ _plugins/pre-calculations.rb
@@ -3,9 +3,9 @@ module SetOfSkills
     def generate(site)
     	tag_counts = []
     	site.tags.each_key do |tagName|
-    		tag_counts << { tag: tagName, count: site.tags[tagName].size, posts: site.tags[tagName]}
+    		tag_counts << [ tagName, site.tags[tagName].size, site.tags[tagName] ]
     	end
-    	tag_counts.sort_by! { |entry| entry[:count] }
+    	tag_counts.sort_by! { |entry| entry[1] }
     	site.data['tag_counts'] = tag_counts.reverse
     end
   end
