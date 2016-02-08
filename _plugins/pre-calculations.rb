9c99777734a26f6efd3b2cb8561b27bf05d5063e
Martin Nordholts <enselic@gmail.com>
2016-02-08 11:23:17 +0100

    wip, posts_by_year precalc

--- _plugins/pre-calculations.rb
+++ _plugins/pre-calculations.rb
@@ -1,12 +1,26 @@
 module SetOfSkills
     class PreCalculations < Jekyll::Generator
         def generate(site)
+            generate_tags_ordered_by_count site
+        end
+
+        def generate_tags_ordered_by_count(site)
         	tags_ordered_by_count = []
         	site.tags.each do |tag, posts| {}
         		tags_ordered_by_count << [ tag, posts.size ]
         	end
         	tags_ordered_by_count.sort_by! { |entry| entry[1] }
         	site.data['tags_ordered_by_count'] = tags_ordered_by_count.reverse
+        end
+
+        def generate_posts_by_year(site)
+            h = Hash.new { |k,v| h[k] = [] }
+            site.posts.each do |post|
+                h[Date.parse(post.date).year] << post
+            end
+            years = h.map { |k,v| [k, v.size, v] }
+            years.sort_by! { |entry| entry[1] }
+            site.data['posts_by_year'] = years
         end
     end
 end
