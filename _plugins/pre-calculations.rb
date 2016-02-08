09999d863866707a31e773bb91d300f72f7c303e
Martin Nordholts <enselic@gmail.com>
2016-02-08 09:18:31 +0100

    Use data html5 attr instead of custom class for tag size

--- _plugins/pre-calculations.rb
+++ _plugins/pre-calculations.rb
@@ -1,12 +1,13 @@
 module SetOfSkills
-  class PreCalculations < Jekyll::Generator
-    def generate(site)
-    	tags_ordered_by_count = []
-    	site.tags.each do |tag, posts| {}
-    		tags_ordered_by_count << [ tag, posts.size ]
-    	end
-    	tags_ordered_by_count.sort_by! { |entry| entry[1] }
-    	site.data['tags_ordered_by_count'] = tags_ordered_by_count.reverse
+    class PreCalculations < Jekyll::Generator
+        def generate(site)
+        	tags_ordered_by_count = []
+        	site.tags.each do |tag, posts| {}
+        		tags_ordered_by_count << [ tag, posts.size ]
+        	end
+        	tags_ordered_by_count.sort_by! { |entry| entry[1] }
+        	site.data['tags_ordered_by_count'] = tags_ordered_by_count.reverse
+        end
     end
-  end
 end
+
