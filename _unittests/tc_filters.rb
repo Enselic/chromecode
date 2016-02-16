58725a2aad9b676243f864cfc1a5184074c3cbd1
Martin Nordholts <enselic@gmail.com>
2016-02-16 09:23:18 +0100

    Add test case that should pass

--- _unittests/tc_filters.rb
+++ _unittests/tc_filters.rb
@@ -4,8 +4,19 @@ require "test/unit"
 class TestSetOfSkillsFilters < Test::Unit::TestCase
 
 	def test_add_noscript_for_lazyload_img()
-		input = "should not be changed"
-		result = SetOfSkillsFilters.add_noscript_for_lazyload_img(input)
-		assert_equal(input, result)
+		tests = [
+			{
+				input: 'should not be changed',
+				expected_result: 'should not be changed'
+			},
+			{
+				input: '<img src="data:image/png;abcdef" data-original="/img/a.png">',
+				expected_result: '<img src="data:image/png;abcdef" data-original="/img/a.png"><noscript><img src="/img/a.png">'
+			}
+		]
+		for tc in tests
+			result = SetOfSkillsFilters.add_noscript_for_lazyload_img(tc[:input])
+			assert_equal(tc[:expected_result], result)
+		end	
 	end
 end
