a9ed41df027312623db62775a1a9464568aa62fa
Martin Nordholts <enselic@gmail.com>
2016-02-16 09:30:04 +0100

    Lets fix noscript imgs later...

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -2,8 +2,8 @@ module SetOfSkillsFilters
     # Adds a 2x3 transparent png data uri to any img tag.
     # For use with img lazy-loading
     def add_noscript_for_lazyload_img(input)
-
-        input.gsub /(<img[^>]+)data-original=([^\s]+)([^>]*>)/, "adf"
+    	# TODO: Fix this. rubular.com is useful help. Don't forget test cases
+        input.gsub /(<img[^>]+)data-original=([^\s]+)([^>]*/?>)/, "adf"
     end
     module_function :add_noscript_for_lazyload_img
 end
