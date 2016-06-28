c4e59098b90ebacc62326fdfcadea291db544dae
Martin Nordholts <enselic@gmail.com>
2016-06-28 14:51:38 +0200

    Start using lazysizes

--- js/main.js
+++ js/main.js
@@ -4,12 +4,6 @@ node_modules/requirejs/bin/r.js -o build.js
 
 */
 
-requirejs.config({
-    shim: {
-        "jquery.lazyload": ["jquery"]
-    }
-});
-
-requirejs(['jquery', 'jquery.lazyload', 'logo-interactivity', 'blog-post-interactivity', 'tags-interactivity'], function($) {
-    $('img[data-original]').lazyload();
+requirejs(['jquery', 'lazysizes', 'logo-interactivity', 'blog-post-interactivity', 'tags-interactivity'], function($) {
+    // no-op
 });
