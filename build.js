3cb82af8144d206f812c7eed6fe4b8c2209fe7cf
Martin Nordholts <enselic@gmail.com>
2016-07-05 09:38:06 +0200

    Build JS, upload to beta bucket

--- build.js
+++ build.js
@@ -5,9 +5,6 @@ node_modules/requirejs/bin/r.js -o build.js
 */
 ({
     baseUrl: "js",
-    shim: {
-        "jquery.lazyload": ["jquery"]
-    },
     name: "main",
     out: "js/main-build-.js" /* manually append with md5 of contents */
 })
